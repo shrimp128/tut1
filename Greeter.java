7bae855275dc95a958587d28ed9a071f5943d33d
Xun <1280068784@qq.com>
2022-09-04 17:29:29 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -18,8 +18,8 @@ public class Greeter {
 
   public static void main(String[] args) {
     String language = args[0];
-    String country = args[1];
-    String name = args[2];
+    String country = args[2];
+    String name = args[1];
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
